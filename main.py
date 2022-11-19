ab736b6230cf907f38176d0d0f6b0c3e736392a5
Lennard Tucholke <lennardtucholke@gmail.com>
2022-11-19 14:01:23 +0100

    FINISHED

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 path_cv = "C:/Users/lenna/Desktop/csv/"
 
 company_name = pd.read_csv(path_cv + 'company_name.csv',
-                 names=('id', 'name', 'country_code', 'imdb_id', 'name_pcode_nf', 'name_pcode_sf', 'md5sum'))
+                           names=('id', 'name', 'country_code', 'imdb_id', 'name_pcode_nf', 'name_pcode_sf', 'md5sum'))
 company_type = pd.read_csv(path_cv + 'company_type.csv', names=('id', 'kind'))
 info_type = pd.read_csv(path_cv + 'info_type.csv', names=('id', 'info'))
 movie_info = pd.read_csv(path_cv + 'movie_info.csv', names=('id', 'movie_id', 'info_type_id', 'info', 'note'))
@@ -14,28 +14,28 @@ movie_info_idx = pd.read_csv(path_cv + 'movie_info_idx.csv', names=('id', 'movie
 title = pd.read_csv(path_cv + 'title.csv', low_memory=False, names=(
     'id', 'title', 'imdb_index', 'kind_ind', 'production_year', 'imdb_id', 'phonetic_code', 'episode_of_id',
     'season_nr', 'episode_nr', 'series_years', 'md5sum'))
-movie_companies = pd.read_csv(path_cv + 'movie_companies.csv', names=('id', 'movie_id', 'company_id', 'company_type_id', 'note'))
+movie_companies = pd.read_csv(path_cv + 'movie_companies.csv',
+                              names=('id', 'movie_id', 'company_id', 'company_type_id', 'note'))
 keyword = pd.read_csv(path_cv + 'keyword.csv', names=('id', 'keyword', 'phonetic_code'))
 link_type = pd.read_csv(path_cv + 'link_type.csv', names=('id', 'link'))
 movie_keyword = pd.read_csv(path_cv + 'movie_keyword.csv', names=('id', 'movie_id', 'keyword_id'))
 movie_link = pd.read_csv(path_cv + 'movie_link.csv', names=('id', 'movie_id', 'linked_movie_id', 'link_type_id'))
 cast_info = pd.read_csv(path_cv + 'cast_info.csv', low_memory=False,
-                 names=('id', 'person_id', 'movie_id', 'person_role_id', 'note', 'nr_order', 'role_id'))
+                        names=('id', 'person_id', 'movie_id', 'person_role_id', 'note', 'nr_order', 'role_id'))
 name = pd.read_csv(path_cv + 'name.csv', low_memory=False, names=(
     'id', 'name', 'imdb_index', 'imdb_id', 'gender', 'name_pcode_cf', 'name_pcode_nf', 'surname_pcode', 'md5sum'))
 aka_name = pd.read_csv(path_cv + 'aka_name.csv', low_memory=False, names=(
     'id', 'person_id', 'name', 'imdb_index', 'name_pcode_cf', 'name_pcode_nf', 'surname_pcode', 'md5sum'))
 char_name = pd.read_csv(path_cv + 'char_name.csv', low_memory=False,
-                  names=('id', 'name', 'imdb_index', 'imdb_id', 'name_pcode_nf', 'surname_pcode', 'md5sum'))
+                        names=('id', 'name', 'imdb_index', 'imdb_id', 'name_pcode_nf', 'surname_pcode', 'md5sum'))
 role_type = pd.read_csv(path_cv + 'role_type.csv', names=('id', 'role'))
-
 
 
 def read_csv(query):
     result = str(db.query(query)).replace("\t", "|").splitlines()[-3].split('|')
     if result.__contains__(''):
         result.remove('')
-    return result
+    return [tuple(result)]
 
 
 if __name__ == '__main__':
@@ -52,6 +52,6 @@ if __name__ == '__main__':
             my_result = read_csv(query)
             cur.execute(query)
             psql_result = cur.fetchall()
-            print(f'my_result:\t{my_result}\t---\tpsql_result:{psql_result}')
+            print(f'Compare {file} query with local and db. Result: {my_result.__eq__(psql_result)}')
         except ValueError:
             print(f'{file} could not execute')
